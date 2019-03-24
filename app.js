5cb46b1e38bbdcfd57950b4ad08ca73cb759d685
Chisimdi Damian Ezeanieto <simdi4iverson@yahoo.co.uk>
2019-03-24 11:40:08 +0100

    Added integration test.

--- app.js
+++ app.js
@@ -12,18 +12,14 @@ const addUser = () => {
   const newUserNameInput = document.querySelector('input#name');
   const newUserAgeInput = document.querySelector('input#age');
 
-  if (
-    !validateInput(newUserNameInput.value, true, false) ||
-    !validateInput(newUserAgeInput.value, false, true)
-  ) {
+  const outputText = checkAndGenerate(newUserNameInput.value,newUserNameInput.value);
+
+  if (!outputText) {
     return;
   }
 
   const userList = document.querySelector('.user-list');
-  const outputText = generateText(
-    newUserNameInput.value,
-    newUserAgeInput.value
-  );
+
   const element = createElement('li', outputText, 'user-item');
   userList.appendChild(element);
 };
--- util.test.js
+++ util.test.js
@@ -1,9 +1,14 @@
-const { generateText } = require('./util.js');
+const { generateText, checkAndGenerate } = require('./util.js');
 
 test('Check if generateText is a function', () => {
     expect(typeof generateText).toBe(`function`);
 });
 test('Should output Max (29 years old)', () => {
     const test = generateText('Max', 29);
+    expect(test).toBe(`Max (29 years old)`);
+});
+
+test('Should generate a valid text output', () => {
+    const test = checkAndGenerate('Max', 29);
     expect(test).toBe(`Max (29 years old)`);
 });

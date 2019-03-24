12c8bc7a0e24edb30b347dc9290c8788de071d97
Chisimdi Damian Ezeanieto <simdi4iverson@yahoo.co.uk>
2019-03-24 12:00:10 +0100

    Done with End to end test.

--- util.test.js
+++ util.test.js
@@ -1,3 +1,4 @@
+const puppeteer = require('puppeteer');
 const { generateText, checkAndGenerate } = require('./util.js');
 
 test('Check if generateText is a function', () => {
@@ -12,3 +13,21 @@ test('Should generate a valid text output', () => {
     const test = checkAndGenerate('Max', 29);
     expect(test).toBe(`Max (29 years old)`);
 });
+
+test('Should click around', async () => {
+    const browser = await puppeteer.launch({
+        headless: false,
+        slowMo: 80,
+        args: ['--window-size=1920, 1080']
+    });
+
+    const page = await browser.newPage();
+    await page.goto('http://127.0.0.1:5500/');
+    await page.click('input#name');
+    await page.type('input#name', 'Anna');
+    await page.click('input#age');
+    await page.type('input#age', '29');
+    await page.click('#btnAddUser');
+    const finalText = await page.$eval('.user-item', el => el.textContent);
+    expect(finalText).toBe('Anna (29 years old)');
+}, 10000);
